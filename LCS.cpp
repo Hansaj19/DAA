06b362491f342e7a74bb5f55b1be35a92f484df0
Hansaj19 <patidarhansaj951@gmail.com>
2025-03-05 13:47:25 +0530

    commited 2nd times , files: kadane , lcs .

---
+++ LCS.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+#include<vector>
+#include<string>
+using namespace std;
+
+void DPtable(string s , string t){
+    int m=s.size();
+    int n=t.size();
+    vector<vector<int>>dp(m+1,vector<int>(n+1,0));
+    for(int i=0;i<=m;i++)dp[i][0]=0;//all (i rows of 0th col)=0.
+    for(int i=0;i<=n;i++)dp[0][i]=0;//all (i cols of 0th row)=0.
+    for(int i=1;i<=m;i++){
+        for(int j=1;j<=n;j++){
+            if(s[i-1]==t[j-1]){
+                dp[i][j]=1+dp[i-1][j-1];//diagonal+1
+            }
+            else{
+                dp[i][j]=max(dp[i-1][j],dp[i][j-1]);//max of(upper,left)
+            }
+        }
+    }
+    int len = dp[m][n];
+    string ans = "";
+    for(int i=0;i<len;i++){
+        ans+='0';
+    }
+    int index = len-1;//0 based indexing
+    int i=m,j=n;
+    while(i>0 && j>0){
+        if(s[i-1]==t[j-1]){
+            ans[index]=s[i-1];
+            index--;
+            i--;j--;
+        }
+        else if(dp[i-1][j]>dp[i][j-1]){
+            i--;
+        }
+        else{
+            j--;
+        }
+    }
+    cout<<ans;
+}
+int main(){
+    string s="hansaj";
+    string t="hansaraj";
+    DPtable(s,t);
+    return 0;
+}
---
+++ kadane.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int kadane(int arr[],int size){
+    int max=INT16_MIN;
+    int sum=0;
+    for(int i=0;i<size;i++){
+        sum +=arr[i];
+        if(sum>max){
+            max=sum;
+        }
+        if(sum<0){
+            sum=0;
+        }
+    }
+    return max;
+}
+int main(){
+    int n;
+    cout<<"enter the szie fo array";
+    cin>>n;
+    int arr[n];
+    for(int i=0; i<n;i++){
+        cin>>arr[i];
+    }
+    int max = kadane(arr,n);
+    cout<<max;
+    return 0;
+}
